9556a86bfd797640eb53ef21cfbd6ee41a1d606a
saloniredij <saloni1829@gmail.com>
2020-08-20 17:58:16 +0530

    initial commit

---
+++ templates/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+	<div class="nav-bar" id="nav-bar">
+		<ol class="nav-bar-obj">
+			<li id="logo"><img src="#"></li>
+			<li id="home"><img src="#"></li>
+			<li id="friends"><img src="#"></li>
+			<li id="chat-box"><img src="#"></li>
+			<li id="search-bar"><img src="#"></li>
+			<li id="nav-bar-emb"><img src="#"></li>
+		</ol>
+	</div>
+
+
+	<div class="club-bar" id="club-bar">
+		<h3>Clubs</h3>
+		<ol>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+
+		</ol>
+	</div>
+
+	<div class="status">
+		<input type="text" name="">
+	</div>
+
+	<div>
+		<ol>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+		</ol>
+	</div>
+
+</body>
+</html>
