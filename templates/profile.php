bb46194b531d8dbe6428a795dbedc67fdbf67970
ASHWATI RAO <61846618+ashwatir@users.noreply.github.com>
2020-08-21 14:08:44 +0530

    Add files via upload

---
+++ templates/profile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../static/profile.css">
+    <script src="https://kit.fontawesome.com/14d91ba717.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="navbar">
+        <div class="logo">
+            <a href="http://"><h3>LOGO</h3></a>
+        </div>
+        <div class="middle">
+            <ul>
+            <li><a href="#"><i class="fas fa-home"></i></a></li>
+            <li><a href="#"><i class="fas fa-users"></i></a></li>
+            <li><a href="#"><i class="fas fa-comment-alt"></i></a></li>
+            </ul>
+            
+        </div>
+        <div class="acc">
+            <ul>
+            <li><i class="fas fa-search"></i></li>
+            <li><a href=""><img class="pfp" src="pfp.jpg" alt=""></li></a>
+            </ul>
+            
+        </div>
+    </div>
+
+</body>
+</html>
