45d08d35eb7d402ee5266871259107efb27319bf
ASHWATI RAO <61846618+ashwatir@users.noreply.github.com>
2020-08-27 11:29:48 +0530

    Add files via upload

---
+++ templates/navbar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../static/navbar.css">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/14d91ba717.js" crossorigin="anonymous"></script>
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,452&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="navbar">
+        <div class="logo">
+            <a href="http://"><h3>LOGO</h3></a>
+        </div>
+        <div class="middle">
+            <ul>
+            <li><a href="#"><i class="fas fa-house-user"></i></a></li>
+            <li><a href="#"><i class="fas fa-users"></i></a></li>
+            <li><a href="#"><i class="fas fa-comment-alt"></i></a></li>
+            <li><a href="#"><i class="fas fa-bell"></i></a></li>
+            </ul>
+            
+        </div>
+        <div class="acc">
+            <ul>
+                <div class="search-box">
+                <input class="search-txt" type="text" name="" placeholder="">
+                <button class="btn-final" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+            
+            <li><button class="corner-img"><img class="pfp" src="../static/pfp.jpg" alt=""></button></li>
+            </ul>
+            
+        </div>
+    </div>
+</body>
