0fbec6a1865dcea1cd3a70aee06cf7cde85f1b05
saloniredij <saloni1829@gmail.com>
2020-09-01 14:26:43 +0530

    connected index and profile page

--- templates/index.php
+++ templates/index.php
@@ -77,8 +77,8 @@
 
                         <div class="post-after">
                             <div class="comment-img"><img src="../static/pfp.jpg" id="comment-img"></div>
-                            <div class="comment"><textarea id="text2" rows="1" cols="62" placeholder="add a comment..." class="comment-text"></textarea></div>
-                            <div class="options"><i class="fas fa-ellipsis-h"></i></div>
+                            <div class="comment"><textarea id="text2" rows="1" cols="250" placeholder="add a comment..." class="comment-text"></textarea></div>
+                            <div class="options"><i class="fas fa-ellipsis-h" ></i></div>
                         </div>
                     </div>
                     
