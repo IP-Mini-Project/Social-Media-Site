b9ee4a4592eae2a7aca774763abc196859ce7a62
ashwatir <ashwatisrao21@gmail.com>
2020-09-06 16:28:31 +0530

    chat

---
+++ templates/chat.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../static/chat.css">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/14d91ba717.js" crossorigin="anonymous"></script>
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,452&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+    <!-- Navbar -->
+    <div id="nav-placeholder">
+
+    </div>
+    <script>
+    $(function(){
+    $("#nav-placeholder").load("navbar.php");
+    });
+    </script>
+    <!-- body -->
+    <div class="container-3">
+        <div class="left">
+            <div class="heading">
+                <h3>CHATS</h3>
+                <h3><i class="fas fa-edit"></i></h3>
+            </div><hr>
+            <div class="sub-head">
+            <div class="group">
+                <h4><i class="fas fa-hashtag"></i> Class of 2020</h4><hr>
+            </div>
+            <div class="club">
+                <h4><i class="fas fa-cannabis"></i> Sports Club</h4> <hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            <div class="name">
+                <h4><i class="fas fa-at"></i> Zendaya</h4><hr>
+            </div>
+            </div>
+            
+            
+            
+        </div>
+        <div class="right">
+            <div class="top-section">
+                <div class="name">
+                    <h2>Zendaya Coleman</h2>
+                </div>
+                <div class="msg-box">
+                    <!-- their msg -->
+                    <div class="their-msg">
+                        <h4>Zendaya Coleman</h4>
+                        <p>hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!</p>
+                    </div>
+                    <div class="their-msg">
+                        <h4>Zendaya Coleman</h4>
+                        <p>hello there!hello there!hello there!hello there!hello there!hello there!hello there!hello there!</p>
+                    </div>
+                    <!-- my msg -->
+                    <div class="my-msg">
+                        <h4>You</h4>
+                        <p>hellooooo hello there!hello there!hello there</p>                                              
+                    </div>
+                    <div class="my-msg">
+                        <h4>You</h4>
+                        <p>hellooooo hello there!hello there!hello there</p>                                              
+                    </div>
+                    <div class="my-msg">
+                        <h4>You</h4>
+                        <p>hellooooo hello there!hello there!hello there</p>                                              
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            <div class="bottom-section">
+                <textarea class="type-msg" name="" id="" cols="76" rows="1"></textarea>
+                <button><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    width="24" height="24"
+                    viewBox="0 0 172 172"
+                    style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#1da1f2"><path d="M0,7.16667v68.08333l136.16667,10.75l-136.16667,10.75v68.08333l172,-78.83333z"></path></g></g></svg>
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
