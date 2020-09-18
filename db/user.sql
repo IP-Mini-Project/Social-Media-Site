89063dcb1f7dc348f2dc1aef39a5574ca715d5c3
saloniredij <saloni1829@gmail.com>
2020-09-18 18:05:15 +0530

    updated db table

--- db/user.sql
+++ db/user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2020 at 01:02 PM
+-- Generation Time: Sep 18, 2020 at 02:34 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -30,29 +30,12 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `gender` char(2) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
-(1, 'victim1', 'kamleshredij@rediffmail.com', '123'),
-(2, 'hs', 'jgg@gmail.com', '123'),
-(4, 'salonii', 'sa829@gmail.com', '159'),
-(7, 's', 'S@gh.com', '123'),
-(8, 's', 'S@gh.com', '123'),
-(9, 's', 'S@gh.com', '123'),
-(10, 's', 'S@gh.com', '123'),
-(11, 'h', 's@gh.com', '123'),
-(12, 's', 'saloni1829@gmail.com', '123'),
-(13, 'Mhealth', 'hfr@gmail.com', '202cb962ac59075b964b07152d234b70'),
-(14, 'sall', 'ffg@jbhv.com', '202cb962ac59075b964b07152d234b70'),
-(15, 'asdf', 'njbb@fgf.com', '202cb962ac59075b964b07152d234b70'),
-(16, 'salo', 'cd@hgh.com', '202cb962ac59075b964b07152d234b70'),
-(17, 'sss', 'tf@RD.COM', '250cf8b51c773f3f8dc8b4be867a9a02');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +55,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
