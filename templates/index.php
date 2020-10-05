317a1f50f698e1dba29a7debfec11e77a605261f
ashwatir <ashwatisrao21@gmail.com>
2020-10-05 21:50:58 +0530

    likes and comment remaining

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,7 @@
-
-
+<?php 
+include("connection.php");
+session_start();
+?>
 
 <html lang="en">
 <head>
@@ -36,24 +38,108 @@
             </div>
             <div class="right-part">
                 <!-- whats happeningarea -->
-                <div class="new-post">
-                    <div class="wrapper">
-                        <div class="newpost-pic">
-                            <img src="../static/pfp.jpg" alt="">
+                
+                    <div class="new-post">
+                        <div class="wrapper">
+                            <div class="newpost-pic">
+                                <img src="../static/pfp.jpg" alt="">
+                            </div>
+                            <form action="index.php" method="post" enctype="multipart/form-data" >
+                            <div class="newpost-text">
+                                <textarea id="text" name="text-box" rows="4" cols="62" placeholder="What's happening?"></textarea>
+                            </div>
                         </div>
-                        <div class="newpost-text">
-                            <textarea id="text" name="text-box" rows="4" cols="62" placeholder="What's happening?"></textarea>
+                        <input type="hidden" name="size" value="1000000">
+                        <div class="endline">
+                        <!-- <i class="fas fa-camera"></i> --> <input type="file" name="image">
+                            <input type="submit" value="POST" class="btn" name="post"> </form>
+                        </div>
+                    </div>
+                
+
+
+                <?php 
+                    $msg = "";
+                    if (isset($_POST['post'])) {
+
+                        $image = $_FILES['image']['name'];
+                    // Get text
+                    $image_text = mysqli_real_escape_string($db, $_POST['text-box']);
+
+                    // image file directory
+                    $target = "../post-images/".basename($image);
+
+                    $sql = "INSERT INTO post (image, text, likes, username) VALUES ('$image', '$image_text','0', '{$_SESSION['username']}')";
+                    // execute query
+                    mysqli_query($db, $sql);
+
+                    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+                        $msg = "Image uploaded successfully";
+                    }else{
+                        $msg = "Failed to upload image";
+                    }
+                }
+                $result = mysqli_query($db, "SELECT * FROM post");
+                    
+                
+                ?>
+                <!-- end of whats happening -->
+                <!-- posts code -->
+                <div class="post-div" style="display: flex;flex-direction: column-reverse;">
+                <?php 
+                
+                while ($row = mysqli_fetch_array($result)) {
+                   $time = $row['time'];
+                   $username= $row['username'];
+                   $text = $row['text'];
+                   $likes = $row['likes'];
+
+
+                   echo " 
+                   <div class='posts'>
+                    <section class='card'>
+                    	<div class='header'>
+                            <div class='prof-img'>
+                             </div>
+                             <div class='prof-info'>
+                                <div class='username'>$username</div>
+                                <div class='location'>Mumbai</div>
+                            </div>
+                            <div class='time'>$time</div>
+                        </div>
+                        <div class='post-img'>
+                            <img src='../post-images/".$row['image']."' class='img'>
+                        </div>
+                        <div class='footer'>
+                        <div class='likes'>
+                            <div class='like-icon'><i class='fas fa-heart'></i></div>
+                            <div class='like-count'>$likes</div>
+                        </div>
+
+                        <div class='description'>
+                            <div class='user'>
+                            	<div class='username'><p>$username</p></div>
+                            	<div class='caption'><p>$text</p></div>
+                            </div>
+                        </div>
+
+                        <div class='post-after'>
+                            <div class='comment-img'><img src='../static/pfp.jpg' id='comment-img'></div>
+                            <div class='comment'><textarea id='text2' rows='1' cols='250' placeholder='add a comment...' class='comment-text'></textarea></div>
+                            <div class='options'><i class='fas fa-ellipsis-h' ></i></div>
                         </div>
                     </div>
                     
-                    <div class="endline">
-                        <i class="fas fa-camera"></i>
-                        <input type="submit" value="POST" class="btn">
-                    </div>
+                    </section>  
+        
+    
+
                 </div>
-                <!-- end of whats happening -->
-                <!-- posts code -->
-                <div class="posts">
+                   ";
+                  }
+                ?>
+                </div>
+                <!-- <div class="posts">
                     <section class="card">
                     	<div class="header">
                             <div class="prof-img">
@@ -91,7 +177,7 @@
         
     
 
-                </div>
+                </div> -->
                 <!-- end of posts code -->
             </div>
         </div>
