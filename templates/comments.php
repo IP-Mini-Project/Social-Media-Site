3515e5f1eb471ed931677399637935c344cb7092
saloniredij <saloni1829@gmail.com>
2020-11-04 19:45:44 +0530

    unfinished logic for comments

---
+++ templates/comments.php
@@ -0,0 +1,18 @@
+<?php 
+	include ('connection.php'); 
+	$get_id = $_GET['post_id'];
+	$get_comment = "select * from comments where post_id = '$get_id' ORDER by 1 DESC";
+
+	$run_comment = mysql_query($db, $get_comment);
+
+	while ($row = mysql_fetch_array($run_comment)) {
+		$comment = $row['comment'];
+		$comment_author = $row['comment_author'];
+		$date = $row['date'];
+
+
+		echo "
+		<div"
+	}
+
+ ?>
