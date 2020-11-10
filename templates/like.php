73a90323ec48e31b4e06cae8793e379ba05c8f5a
saloniredij <saloni1829@gmail.com>
2020-11-10 19:51:38 +0530

    renamed notif.php to likes and added connectivity to likes table

--- templates/like.php
+++ templates/like.php
@@ -1,12 +1,23 @@
 <?php
- include ('connection.php'); 
+ include ('server.php'); 
  $type=$_POST['type'];
 $id=$_POST['id'];
 if($type=='like'){
  $sql="UPDATE post SET likes=likes+1 WHERE id = $id ";
 }
-else{
- $sql="UPDATE post SET likes=likes-1 WHERE id = $id";
-}
+// else{
+//  $sql="UPDATE post SET likes=likes-1 WHERE id = $id";
+// }
  mysqli_query($db,$sql);
+
+if (isset($_POST['type'])) {
+	
+	$username = $_SESSION['username'];
+
+$query = "INSERT INTO likes (post_id,username) VALUES ('$id','$username')";
+	mysqli_query($db,$query);
+	# code...
+}
+
+
  ?>
